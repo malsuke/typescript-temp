c24ac1680d2663075e72ed73f5498beb33fcc0e7
malsuke <malsuke096@gmail.com>
2025-11-01 00:41:52 +0900

    rename: file rename

---
+++ .eslint.config.js
@@ -0,0 +1,45 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  {
+    ignores: ['dist', 'dist-cjs', 'dist-esm'],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...compat.extends('airbnb-base'),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
+      ],
+      'import/prefer-default-export': 'off',
+      'newline-before-return': 'error',
+      'no-console': 'off',
+      'no-var': 'error',
+    },
+  },
+  eslintConfigPrettier,
+];
